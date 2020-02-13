8c3cc65b609a5aca85d2e9624ade894850fce874
patrykmaron <up705461@myport.ac.uk>
2020-02-13 22:18:19 +0000

    add purgecss, added cross-env, improved build

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,16 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-env node */
 //postcss.config.js
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const tailwindcss = require("tailwindcss");
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./src/**/*.html", "./src/**/*.tsx", "./src/**/*.jsx"],
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
 module.exports = {
-  plugins: [tailwindcss("./tailwind.js"), require("autoprefixer")]
+  plugins: [
+    tailwindcss("./tailwind.config.js"),
+    require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
+  ]
 };
